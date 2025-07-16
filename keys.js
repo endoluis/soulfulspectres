b6287c1f8390d769e99b82794e6520bc5cf81d33
endoluis <soulfulspectres@gmail.com>
2025-07-16 23:26:17 +0100

    Delete keys.js

--- keys.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    const logo = document.getElementById('logo');
-    if (logo) {
-        logo.addEventListener('click', () => {
-            window.location.href = 'inicio.html'; // Página futura del laberinto
-        });
-    }
-});
